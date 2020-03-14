38905f179f5a8de3dcd5b713fa89f0c88d519f07
therealsharath <palathingal@gatech.edu>
2020-03-14 19:34:59 -0400

    Updated Main

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import logo from './vybeslogo.png';
 import Navbar from './components/navbar/Navbar.js';
+import Body from './components/body/Body.js';
+import Content from './components/content/Content.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Body />
+      <Content />
     </div>
   );
 }
